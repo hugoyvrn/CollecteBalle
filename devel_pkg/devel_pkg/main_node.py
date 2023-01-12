b4503e5fdb64f865b0042e72ee29f40c217b8198
Damien SNLT <damien.esnault@ensta-bretagne.org>
2023-01-12 10:52:58 +0100

    add a display window to see the simulation

--- devel_pkg/devel_pkg/main_node.py
+++ devel_pkg/devel_pkg/main_node.py
@@ -11,17 +11,27 @@ from sensor_msgs.msg import Image
 class ImageParser(Node):
 
     def __init__(self):
+        # Create node
         super().__init__('image_parser')
-        self.subscription = self.create_subscription(Image,'/zenith_camera/image_raw',self.listener_callback,10)
-        self.subscription
-        self.image = image
+        # Create subscriber
+        self.subscription = self.create_subscription(Image,'/zenith_camera/image_raw',self.get_image_callback,10)
+        self.subscription # Avoid warning unused variable
+        # Create variable to store image
+        self.image = np.zeros((240,240,3))
+        # Create variable to save position of ball
+        self.positions = []
 
-    def listener_callback(self, img_msg):
+    def get_image_callback(self, img_msg):
         # Note: get encoding but for our case its rbg8
         height, width, encoding = img_msg.height, img_msg.width, img_msg.encoding
-        image = np.array(img_msg.data).reshape((height,width,3))
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        self.image = image
+        # Store image and 
+        self.image = np.array(img_msg.data).reshape((height,width,3))
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_RGB2BGR)s
+        self.detect_ball_in_image()
+    
+    def detect_ball_in_image(self):
+        cv2.imshow("Court de tennis",self.image)
+        cv2.waitKey(1)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -30,6 +40,7 @@ def main(args=None):
 
     rclpy.spin(image_parser)
 
+    cv2.destroyAllWindows()
     image_parser.destroy_node()
     rclpy.shutdown()
 
